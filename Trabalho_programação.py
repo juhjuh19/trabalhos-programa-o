a5ac671779c98291c03c76d357dbf399e50b662d
Juliana <julianaife1234@gmail.com>
2025-11-02 22:25:59 -0400

    Atualizar o Trabalho_programação.py

--- Trabalho_programação.py
+++ Trabalho_programação.py
@@ -14,18 +14,18 @@ if ph_atual < ph_limite_acido:
     diferenca_ph = 7.0 - ph_atual
     volume_neutralizante = diferenca_ph * fator_base
 
-    print(f"STATUS: ÁCIDO (pH {ph_atual:.2f}). Necessita de BASE.")
-    print(f"Ação: Dosar {volume_neutralizante:.2f} Litros de agente neutralizante BÁSICO.")
+    print(f"Status: ÁCIDO (pH {ph_atual:.2f}). Necessita de Base.")
+    print(f"Ação: Dosar {volume_neutralizante:.2f} Litros de agente neutralizante Básico.")
 
 # Caso 2: Solução Básica (pH acima do limite básico)
 elif ph_atual > ph_limite_basico:
     diferenca_ph = ph_atual - 7.0
     volume_neutralizante = diferenca_ph * fator_acido
 
-    print(f"STATUS: BÁSICO (pH {ph_atual:.2f}). Necessita de ÁCIDO.")
-    print(f"Ação: Dosar {volume_neutralizante:.2f} Litros de agente neutralizante ÁCIDO.")
+    print(f"Status: BÁSICO (pH {ph_atual:.2f}). Necessita de Ácido.")
+    print(f"Ação: Dosar {volume_neutralizante:.2f} Litros de agente neutralizante Ácido.")
 
 # Caso 3: pH na faixa aceitável
 else:
-    print(f"STATUS: ACEITÁVEL. O pH {ph_atual:.2f} está na faixa de tolerância.")
+    print(f"Status: ACEITÁVEL. O pH {ph_atual:.2f} está na faixa de tolerância.")
     print("Ação: Manter monitoramento. Nenhuma dosagem é necessária no momento.")
